629eb3738734fd9f899aee69ca1ee7eab8bddb57
Hamdani Gandi Alhassan <hamdani@cloudplexo.com>
2026-02-18 20:34:17 +0000

    Refactor codebase

--- apps/api/.migrationrc.js
+++ apps/api/.migrationrc.js
@@ -1,7 +1,5 @@
-const { getDatabaseUrl } = require('@lusilearn/config');
-
 module.exports = {
-  'database-url': getDatabaseUrl(),
+  'database-url': process.env.DATABASE_URL || `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
   'migrations-dir': 'src/database/migrations',
   'migrations-table': 'pgmigrations',
   'schema': 'public',
